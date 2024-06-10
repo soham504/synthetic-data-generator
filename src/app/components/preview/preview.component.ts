07c5d580201a30b9e5882ecf93797a7fb4536bee
Arunabha-NOTE <arunabham2017@gmail.com>
2024-06-10 17:58:16 +0530

    added table remarks area and submit button

--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {TableModule} from "primeng/table";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-preview',
   standalone: true,
-  imports: [],
+    imports: [TableModule, CommonModule,FormsModule, InputTextareaModule,ButtonModule],
+    // providers: [ProductService],
   templateUrl: './preview.component.html',
   styleUrl: './preview.component.scss'
 })
 export class PreviewComponent {
-
+    value!: string;
+    // products!: Product[];
+    //
+    // constructor(private productService: ProductService) {}
+    //
+    // ngOnInit() {
+    //     this.productService.getProductsMini().then((data) => {
+    //         this.products = data;
+    //     });
+    // }
 }
