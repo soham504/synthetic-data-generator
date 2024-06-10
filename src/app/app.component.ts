ca90a8c86fc2fb0a81b30c7af71e9e6797d7473f
Arunabha-NOTE <arunabham2017@gmail.com>
2024-06-10 15:38:25 +0530

    made upload button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {UploadButtonComponent} from "./components/upload-button/upload-button.component";
 import {PreviewComponent} from "./components/preview/preview.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, NavbarComponent, UploadButtonComponent, PreviewComponent],
+    imports: [RouterOutlet, NavbarComponent, UploadButtonComponent, PreviewComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/upload-button/upload-button.component.ts
+++ src/app/components/upload-button/upload-button.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FileUploadEvent, FileUploadModule} from "primeng/fileupload";
+import {CommonModule, NgForOf, NgIf} from "@angular/common";
+import {ToastModule} from "primeng/toast";
+import {MessageService} from "primeng/api";
+
+interface UploadEvent {
+    originalEvent: Event;
+    files: File[];
+}
 
 @Component({
-  selector: 'app-upload-button',
-  standalone: true,
-  imports: [],
-  templateUrl: './upload-button.component.html',
-  styleUrl: './upload-button.component.scss'
+    selector: 'app-upload-button',
+    standalone: true,
+    imports: [FileUploadModule, ToastModule, CommonModule],
+    providers: [MessageService],
+    templateUrl: './upload-button.component.html',
+    styleUrl: './upload-button.component.scss'
 })
 export class UploadButtonComponent {
+    uploadedFiles: any[] = [];
 
+    constructor(private messageService: MessageService) {
+    }
+
+    onUpload(event: FileUploadEvent) {
+        for (let file of event.files) {
+            this.uploadedFiles.push(file);
+        }
+
+        this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});
+    }
 }
