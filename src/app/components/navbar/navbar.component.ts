5e03bebdcbce94a785aaec150242a26c8de84833
Arunabha-NOTE <arunabham2017@gmail.com>
2024-06-10 13:15:25 +0530

    navbar styling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import {BadgeModule} from "primeng/badge";
+import {NgClass, NgIf} from "@angular/common";
+import {AvatarModule} from "primeng/avatar";
+import {Ripple} from "primeng/ripple";
+import {InputTextModule} from "primeng/inputtext";
+import {MenuItem} from "primeng/api";
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [MenubarModule, BadgeModule, NgClass, NgIf, AvatarModule, Ripple, InputTextModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+    items!: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                url : 'http://localhost:4200/'
+
+            },
+        ];
+    }
 
 }
