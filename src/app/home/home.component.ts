725a001320e52377e110e26b40b11e9e99809f92
Arunabha-NOTE <arunabham2017@gmail.com>
2024-06-16 17:19:19 +0530

    added sample json button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import {PreviewComponent} from "../components/preview/preview.component";
 import {UploadButtonComponent} from "../components/upload-button/upload-button.component";
+import {Button, ButtonDirective} from "primeng/button";
+import {Ripple} from "primeng/ripple";
 
 @Component({
   selector: 'app-home',
   standalone: true,
     imports: [
         PreviewComponent,
-        UploadButtonComponent
+        UploadButtonComponent,
+        ButtonDirective,
+        Ripple,
+        Button
     ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
     constructor() {
+    }
+
+    handleClick() {
+        console.log('button clicked')
     }
 }
